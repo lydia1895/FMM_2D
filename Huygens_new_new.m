b1282a1fac20e9397fe61fde065002c023b30e47
Lydia <lydia.pogorelskaya@gmail.com>
2018-04-02 17:24:15 +0300

    perovskite_whiskers

--- Huygens_new_new.m
+++ Huygens_new_new.m
@@ -4,13 +4,13 @@ clear all
 
 N = 3;                %number of Fourier orders
 L = 2;                 %number of layers
-periodx = 360;  %period of periodic layer
-periody = 360;  %period of periodic layer
-r = 130;        %disc radius
+periodx = 420;  %period of periodic layer
+periody = 420;  %period of periodic layer
+r = 184;        %disc radius
 a = periodx;  
 h = zeros(L,1);
 h(2) = 250;
-h(1) = 360;       %thickness of periodic layer
+h(1) = 315;       %thickness of periodic layer
 
 M = 501;               %number of modes for Fourier transform of epsilon
 Mr = (r/a)*M;
@@ -18,22 +18,20 @@ Mr = (r/a)*M;
 i0 = 1+floor(M/2);
 j0 = 1+floor(M/2);
 %{
-a = 360
-H = 360
-R = 130
-n1 = n2 = 1.46
-
-???????? ?? 1300 ?? 1700, ???? ?? 35 ?? 80 ????????.
-
-????? 4 ???????? ? ??????? eps: ????????? ? ???? ??? ?? ? ?? ???????????.
+a = 420
+H = 315
+R = 184
+n1 = 1
+n2 = 1.46
+????????: ?? 1350 ?? 1800, ?? 35 ?? 80. 
 
 %}
-lmin = 1300;
-lmax = 1700;
-Nl=81;
+lmin = 1350;
+lmax = 1800;
+Nl=91;
 lambda = linspace(lmin,lmax,Nl);
 
-n_media = 1.46;
+n_media = 1.0;
 eps_media = n_media^2;
 n_prism = 2.3;
 eps_prism = n_prism^2;
@@ -121,7 +119,7 @@ end
 
 phase_R = zeros(Nl,Nt);
 Rsum_full = zeros(Nl,Nt);
-polarization = 'TM';
+polarization = 'TE';
 
 for i=1:Nl
     
@@ -154,21 +152,20 @@ for i=1:Nl
     lambda(i)
 end
 %{
-a = 360
-H = 360
-R = 130
-n1 = n2 = 1.46
-
-???????? ?? 1300 ?? 1700, ???? ?? 35 ?? 80 ????????.
+a = 420
+H = 315
+R = 184
+n1 = 1
+n2 = 1.46
 %}
 %save('MyMatrix.txt', 'A', '-ascii', '-double', '-tabs')
 %llambda = transpose(lambda);
 %ttheta = transpose(theta);
 %lt = cat(2,llambda,ttheta);
-save('TM_R_a_360_H_360_R_130_n1_1_46_n2_1_46.txt', 'Rsum', '-ascii', '-double', '-tabs')
-save('TM_phase_a_360_H_360_R_130_n1_1_46_n2_1_46.txt', 'phase_R','-ascii',  '-double', '-tabs')
-save('TM_lambda_theta_a_360_H_360_R_130_n1_1_46_n2_1_46.txt', 'lambda', 'theta','-ascii', '-double', '-tabs')
-save('TM_a_360_H_360_R_130_n1_1_46_n2_1_46.mat', 'Rsum', 'phase_R', 'lambda', 'theta')
+save('TE_R_a_420_H_315_R_184_n1_1_n2_1_46.txt', 'Rsum', '-ascii', '-double', '-tabs')
+save('TE_phase_a_420_H_315_R_184_n1_1_n2_1_46.txt', 'phase_R','-ascii',  '-double', '-tabs')
+save('TE_lambda_theta_a_420_H_315_R_184_n1_1_n2_1_46.txt', 'lambda', 'theta','-ascii', '-double', '-tabs')
+save('TE_a_420_H_315_R_184_n1_1_n2_1_46.mat', 'Rsum', 'phase_R', 'lambda', 'theta')
 %{
 figure(1)
 plot(lambda, Rsum, 'g', lambda, Tsum, 'r', 'Linewidth', 2);
