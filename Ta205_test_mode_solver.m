3431b12ddba01036fdfd96a325d5dca3c3ebb774
Lydia <lydia.pogorelskaya@gmail.com>
2018-12-04 17:29:43 +0300

    ok

--- Ta205_test_mode_solver.m
+++ Ta205_test_mode_solver.m
@@ -4,7 +4,7 @@ clear all
 N = 5;                %number of Fourier orders
 periodx = 490*10^(-9);
 dx = 145*10^(-9);
-periody = 700*10^(-9);
+periody = 650*10^(-9);
 L = 2;
 h = zeros(L,1);
 h(2) = 90*10^(-9);
@@ -39,7 +39,7 @@ thetamax = 1*pi/180;
 theta = linspace(thetamin,thetamax,20);
 [Ntt,Nt] = size(theta);
 %}
-l1 = 760*10^(-9);
+l1 = 735*10^(-9);
 dl = 0.05*10^(-9);
 l2 = l1+dl;
 lambda = [l1 l2];
@@ -49,8 +49,10 @@ w2 = c/l2;
 dw = w2-w1;
 Nl = 2;
 
-theta = 4*pi/180;
-Nt = 1;
+thetamax = 5*pi/180;
+thetamin = 0*pi/180;
+theta = linspace(thetamax,thetamin,11);
+[Ntt,Nt] = size(theta);
 
 phi = 0*pi/180;
 Np=1;
@@ -67,14 +69,17 @@ for i=1:L
 [eps11(:,:,i), eps22(:,:,i), eps33(:,:,i)] = FMM_eps123_new(epsilon(:,:,i),N,M);
 end
 polarization = 'TE';
-
 N_iterations = 10;
 
-for ii=1:N_iterations
+w_eig = zeros(Nt,1);
+dw00 = zeros(N_iterations, Nt);
+
+for j=1:Nt
+    for k=1:Np
+        for ii=1:N_iterations
     
-    for i=1:Nl
-        for j=1:Nt
-            for k=1:Np
+            for i=1:Nl
+        
                 [R] = FMM_1D_TE_RT_multi_mode_solver(eps11,eps22,eps33,...
                     periodx, periody, h, lambda(i), theta(j), phi(k), refIndices, N, M, L, polarization);
             
@@ -84,25 +89,42 @@ for ii=1:N_iterations
                     R1 = R;
                 end
             end
+            dR = (R1-R0)/dw;
+            MAXDR(ii) = max(dR(:));
+            
+            A=-dR\R0;
+            [V, D] = eig(A);
+            
+            w_array = diag(D);
+            dw0 = min(w_array);
+            
+            w1 = w1 + dw0;
+            w2 = w1 + dw;
+            
+            l1 = c/w1;
+            l2 = c/w2;
+            lambda = [l1 l2];
+            
+            dw00(ii,j) = dw0;
         end
+        w_eig(j) = w1;
     end
-    dR = (R1-R0)/dw;
-    MAXDR(ii) = max(dR(:));
-            
-    A=-dR\R0;
-    [V, D] = eig(A);
-            
-    w_array = diag(D);
-    dw0 = min(w_array);
-            
-    w1 = w1 + dw0;
-    w2 = w1 + dw;
-            
-    l1 = c/w1;
-    l2 = c/w2;
-    lambda = [l1 l2];
-            
-    dw00(ii) = dw0;
+    
 end
-l1
+lambda_eig = c./w_eig;
+k0=2*pi./real(lambda_eig);
+kx = k0*periodx*sin(theta')*cos(phi);
+plot(kx,w_eig,'b','Linewidth',2);
 
+theta_new = theta*180/pi;
+%plot(theta_new, lambda_eig*10^9)
+plot(lambda_eig*10^9,theta_new)
+%{
+k0=2*pi/lambda;
+kx = sin(theta')*cos(phi);
+ky = sin(theta')*sin(phi);
+figure;
+pcolor(kx,ky,Rsum)
+%shading interp
+colormap gray;
+%}
--- offBIC_mode_solver.m
+++ offBIC_mode_solver.m
@@ -1,45 +1,35 @@
 clc
 clear all
 
-N = 5;                %number of Fourier orders
-periodx = 490*10^(-9);
-dx = 145*10^(-9);
-periody = 700*10^(-9);
-L = 2;
+N = 6;                %number of Fourier orders
+
+L = 1;
 h = zeros(L,1);
-h(2) = 90*10^(-9);
-h(1) = 1*10^(-6);
-M = 5001;
+th = 212*10^(-9);
+h(1) = th;
+periodx = th;
+dx = periodx*0.45;
+periody = th;
+M = 3001;
 x = (1:1:M)*periodx/M;
 epsilon = zeros(M, M, L);
 
 
-nTa2O5 = 2.1;
-epsTa2O5 = nTa2O5^2;
-nSiO2 = 1.48;
-epsSiO2 = nSiO2^2;
-nSi = 3.71;
-epsilon(:,:,2)=epsTa2O5*ones(M,M,1);
-epsilon(:,:,1)=epsSiO2*ones(M,M,1);
-refIndices = [1.0 nSi];  
+nlattice = 2.125;
+epslattice = nlattice^2;
+nmedia = 1.46;
+epsmedia = nmedia^2;
+epsilon(:,:,1)=epsmedia*ones(M,M,1);
+refIndices = [nmedia nmedia];  
 for i=1:M
     for j=1:M
     if x(i)<dx
-        epsilon(j,i,2) = 1.0;
+        epsilon(j,i,2) = epslattice;
     end
     end
 end
-%{
-lmin = 735*10^(-9);
-lmax = 765*10^(-9);
-lambda = linspace(lmin, lmax, 80);
-[Nll,Nl] = size(lambda);
-thetamin = 0*pi/180;
-thetamax = 1*pi/180;
-theta = linspace(thetamin,thetamax,20);
-[Ntt,Nt] = size(theta);
-%}
-l1 = 760*10^(-9);
+
+l1 = 489*10^(-9);
 dl = 0.05*10^(-9);
 l2 = l1+dl;
 lambda = [l1 l2];
@@ -49,10 +39,13 @@ w2 = c/l2;
 dw = w2-w1;
 Nl = 2;
 
-thetamax = 4*pi/180;
-thetamin = 0*pi/180;
-theta = linspace(thetamax,thetamin,5);
-[Ntt,Nt] = size(theta);
+kx = 9.06*10^6;
+theta = asin(kx*l1/(2*pi));
+%thetamax = thetamin + 1*pi/180;
+Nt = 1;
+%theta = linspace(thetamax,thetamin,6);
+%[Ntt,Nt] = size(theta);
+
 
 phi = 0*pi/180;
 Np=1;
@@ -68,10 +61,10 @@ eps33=zeros(P*Q,P*Q,L);
 for i=1:L
 [eps11(:,:,i), eps22(:,:,i), eps33(:,:,i)] = FMM_eps123_new(epsilon(:,:,i),N,M);
 end
-polarization = 'TE';
-N_iterations = 4;
+polarization = 'TM';
+N_iterations = 12;
 
-w_eig = zeros(Nt);
+w_eig = zeros(Nt,1);
 dw00 = zeros(N_iterations, Nt);
 
 for j=1:Nt
@@ -111,10 +104,14 @@ for j=1:Nt
     end
     
 end
-lambda = c./w_eig;
-k0=2*pi./lambda;
+lambda_eig = c./w_eig;
+k0=2*pi./real(lambda_eig);
 kx = k0*periodx*sin(theta')*cos(phi);
 plot(kx,w_eig,'b','Linewidth',2);
+
+theta_new = theta*180/pi;
+%plot(theta_new, lambda_eig*10^9)
+plot(lambda_eig*10^9,theta_new)
 %{
 k0=2*pi/lambda;
 kx = sin(theta')*cos(phi);
