2853a8fc9064fae4824b84fa90a93491db3bd8ee
Lydia <lydia.pogorelskaya@gmail.com>
2019-03-04 17:52:16 +0300

    for A and B

--- offBIC_test.m
+++ offBIC_test.m
@@ -1,22 +1,22 @@
 clc
 clear all
 
-N=3;
+N = 3;
 L = 1;
 h = zeros(L,1);
-th = 212*10^(-9);
+th = 100;
 h(1) = th;
-periodx = 1.8*th;
-dx = 0.45*periodx;
+periodx = 1000;
+dx = 500;
 periody = periodx;
 M = 3001;
 x = (1:1:M)*periodx/M;
 epsilon = zeros(M, M, L);
 
 
-nlattice = 2.08;
+nlattice = 1.44;
 epslattice = nlattice^2;
-nmedia = 1.46;
+nmedia = 1.0;
 epsmedia = nmedia^2;
 epsilon(:,:,1)=epsmedia*ones(M,M,1);
 refIndices = [nmedia nmedia];  
@@ -31,8 +31,8 @@ end
 
 %lmin = periodx+300*10^(-9);
 %lmax = periodx+600*10^(-9);
-lmin = 0.71*10^(-6);
-lmax = 0.81*10^(-6);
+lmin = 950;
+lmax = 1050;
 lambda = linspace(lmin, lmax, 100);
 [Nll,Nl] = size(lambda);
 
@@ -40,9 +40,9 @@ lambda = linspace(lmin, lmax, 100);
 kx = 3.14*10^6;
 l1 = 825*10^(-9);
 theta1 = asin(kx*l1/(2*pi*nmedia))
-thetamin = 12*pi/180;
-thetamax = 24*pi/180;
-theta = linspace(thetamin,thetamax,36);
+thetamin = 0*pi/180;
+thetamax = 3*pi/180;
+theta = linspace(thetamin,thetamax,30);
 [Ntt,Nt] = size(theta);
 
 phi = 0*pi/180;
@@ -60,7 +60,7 @@ eps33=zeros(P*Q,P*Q,L);
 for i=1:L
 [eps11(:,:,i), eps22(:,:,i), eps33(:,:,i)] = FMM_eps123_new(epsilon(:,:,i),N,M);
 end
-polarization = 'TM';
+polarization = 'TE';
 for i=1:Nl
     i
     for j=1:Nt
@@ -75,10 +75,6 @@ for i=1:Nl
     end
 end
 
-
-lambda = lambda*10^6;
-lmin = lmin*10^6;
-lmax = lmax*10^6;
 
 figure(5)
 hold on
@@ -137,5 +133,22 @@ colorbar;
 %caxis([0 0.4])
 hold off
 
+
+c = physconst('LightSpeed');
+h = 4.135666 * 10^(-15);
+kx = (2*periodx./lambda)'*sin(theta);
+frequency = (periodx./lambda');
+
+figure(2);
+pcolor(kx,frequency,Rsum)
+xlabel('kx, \pi/a');
+ylabel('frequency, THz');
+colormap('jet');
+colorbar;
+set(gca,'fontsize', 16)
+shading flat
+caxis([0 1])
+colorbar
+hold on
 %save('period_th_1_8_nlattice_1_608.mat','theta','lambda','Rsum','nlattice','nmedia','periodx','dx')
 
